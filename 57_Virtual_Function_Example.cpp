b8e804c735b4a53d57a9be2708f99ae9da478ac5
Raj Randive <randiveraj96@gmail.com>
2022-09-01 22:13:59 +0530

    initial commit

---
+++ 57_Virtual_Function_Example.cpp
@@ -0,0 +1,104 @@
+// Virtual Function Example
+
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class CWH{
+
+    protected:
+        string title;
+        float rating;
+
+    public:
+        CWH(string s, float r){
+            title = s;
+            rating = r;
+        }
+
+        // AGAR HUM YE VIRTUAL NAHI LAGATE HAI THEN BASE CLASS KA HI DISPLAY RUN HOGA HAR BAR. JUST CHECK THIS THING BY REMOVING THE VIRTUAL KEYWORD...!!
+        virtual void display(){
+            cout << "Running Display() of Base Class"<<endl;
+        }
+};
+
+class CWHVideo : public CWH{
+
+    float videoLength;
+    public:
+        CWHVideo(string s, float r, float vl) : CWH(s, r){
+            videoLength = vl;
+        }
+
+        void display(){
+            cout << "This is an amazing video with title " << title << endl;
+            cout << "Ratings: " << rating << " out of 5 stars." << endl;
+            cout << "Length of this video is: " << videoLength << " minutes." << endl;
+        }
+
+};
+
+class CWHText : public CWH{
+
+    int words;
+    public:
+        CWHText(string s, float r, int wc) : CWH(s, r){
+            words = wc;
+        }
+
+
+        // AGAR HUMNE ISKO POINTER KI MADAD SE CALL KIYA HAI AND AGAR HUMNE DISPLAY FUNCTION NAHI BANAYA HAI FOR THIS CLASS THEN IT WILL RUN THE DISPLAY FUNCTION OF THE BASE CLASS. MATLAB JARURI NAHI HAI KI DERIVED CLASS ME VO FUNCTION BANA HAI.
+
+        // void display(){
+        //     cout << "This is an amazing text with title " << title << endl;
+        //     cout << "Ratings of this text tutorial: " << rating << " out of 5 stars." << endl;
+        //     cout << "Number of words in this text tutorial is: " << words << " words." << endl;
+        // }
+};
+
+
+int main()
+{
+
+    string title;
+    float rating, vlen;
+    int words;
+
+    //for CWH Video
+    title = "Django Tutorial";
+    vlen = 4.56;
+    rating = 4.89;
+
+    cout << "\n";
+    CWHVideo djVideo(title, rating, vlen);
+    djVideo.display();
+    cout << "\n";
+
+
+    //for CWH Text
+    title = "Django Tutorial Textual";
+    words = 499;
+    rating = 4.13;
+
+    cout << "\n";
+    CWHText djText(title, rating, words);
+    djText.display();
+    cout << "\n";
+
+
+    // ******************************************************
+    CWH * tuts[2]; // Do pointers banaye hai
+    tuts[0] = &djVideo;  // Object of Video class
+    tuts[1] = &djText;   // Object of Text class
+
+ 
+    cout << "\n";
+    cout << "****************************************************************"<<endl;
+    tuts[0] -> display();
+    cout << "\n";
+    tuts[1] -> display();
+    
+
+    return 0;
+}
